61814d515f081e310f63e03072bc0eff6b127f7f
SmilingWitch <danielitayero@gmail.com>
2024-02-21 11:15:59 -0500

    feat: add form to Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,21 +5,16 @@ export default function Contact(){
         <div className={style.contact}>
             <div className={style.bx}>
                <h1>Contact Me</h1>
-
                 <form className={style.form}>
                     <div  className={style.inputBx}>
                         <input type = "text" placeholder="Your name"></input>
                         <input type = "email" placeholder="Your email address"></input>
                         <input type = "text" placeholder="Subject"></input>
-
                     </div>
 
                     <div className={style.message}>
-                        <input type = "message"></input>
-
+                        <input type = "text" placeholder="Type here your message"></input>
                     </div>
-                    
-
                 </form>
 
             </div>
