9dd66a9b1affec0ac5dfe777eb5f2d63a038ff18
SmilingWitch <danielitayero@gmail.com>
2024-02-20 21:47:08 -0500

    feat: add Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import style from "../../public/styles/Contact.module.css"
+
+export default function Contact(){
+    return(
+        <div className={style.contact}>
+            <div className={style.bx}>
+               <h1>Contact Me</h1>
+
+                <form className={style.form}>
+                    <div  className={style.inputBx}>
+                        <input type = "text" placeholder="Your name"></input>
+                        <input type = "email" placeholder="Your email address"></input>
+                        <input type = "text" placeholder="Subject"></input>
+
+                    </div>
+
+                    <div className={style.message}>
+                        <input type = "message"></input>
+
+                    </div>
+                    
+
+                </form>
+
+            </div>
+            
+        </div>
+    )
+}
