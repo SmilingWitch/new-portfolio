3292b02e08537edbc37b60efc0afe24c72d11917
SmilingWitch <danielitayero@gmail.com>
2024-03-19 11:58:16 -0400

    feat: add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import style from "../../public/styles/Navbar.module.css"
+
+export default function Navbar(){
+    return(
+        <div className={style.navbar}>
+            <div className={style.bx}>
+                Navbar
+
+            </div>
+
+        </div>
+    )
+}
